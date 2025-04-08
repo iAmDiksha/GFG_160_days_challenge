73e18618a87ebcfb6fef46978ab94a483739afd0
Diksha <jdiksha394@gmail.com>
2025-04-08 11:36:04 +0530

    Day 14 added

---
+++ Day14.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int myAtoi(char *s) {
+        // Your code here
+        int res = 0;
+        int sign = 1;
+        int idx = 0;
+        
+        while(s[idx] == ' ')
+        {
+            idx++;
+        }
+        
+        if(s[idx] == '+' || s[idx] == '-')
+        {
+            if(s[idx] == '-') sign = -1;
+            idx++;
+        }
+        
+        while(s[idx] >= '0' && s[idx] <= '9')
+        {
+            if(res > INT_MAX/10 || (res == INT_MAX/10 && s[idx]-'0' > 7)){
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+            
+            res = 10*res + (s[idx++] - '0');
+        }
+        
+        return res*sign;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        char s[20];
+        cin >> s;
+        Solution ob;
+        int ans = ob.myAtoi(s);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends
