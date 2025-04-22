457fe8ef1fcb756f401c561e18b8f55da77f1503
Diksha <jdiksha394@gmail.com>
2025-04-22 17:03:51 +0530

    day 28 added

---
+++ Searching/Day28.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        int l = lower_bound(arr.begin(),arr.end(),target) - arr.begin();
+        int r = upper_bound(arr.begin(),arr.end(), target) - arr.begin();
+        return r-l;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.countFreq(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
